7080c7eef21d6824cf41a65cadbfbd90dcf76727
Duy Hung <daoduyhung98@gmailcom>
2024-10-04 08:24:32 +0700

    first commit

--- survey_dienbien/models/survey_user_input.py
+++ survey_dienbien/models/survey_user_input.py
@@ -5,9 +5,9 @@ class SurveyUserInput(models.Model):
     _inherit = 'survey.user_input'
 
     name = fields.Char(string='Name', required=False,compute='_compute_name', store=True)
-    diem_cao_nhat = fields.Integer('Điểm cao nhất', readonly=True)
+    diem_cao_nhat = fields.Integer('Điểm cao nhất', readonly=True, compute='_compute_diem_cao_nhat', store=True)
     so_lan_lam = fields.Integer(string='Tổng số lượt thi', readonly=True, compute='_compute_so_lan_lam', store=True)
-    xep_loai = fields.Char(string='Xếp loại', required=False, readonly=True,
+    xep_loai = fields.Char(string='Xếp loại', required=False, readonly=True,compute='_compute_xep_loai', store=True,
                            help="Xếp loại đựa trên điểm cao nhất")
     partner_name_clean = fields.Char(string='Tên đối tác chuẩn hóa',compute='_compute_partner_name_clean',store=True
     )
@@ -18,27 +18,36 @@ class SurveyUserInput(models.Model):
             # Chuẩn hóa tên đối tác: loại bỏ khoảng trắng và chuyển thành chữ thường
             record.partner_name_clean = record.partner_id.name.strip().lower() if record.partner_id.name else ''
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        records = super(SurveyUserInput, self).create(vals_list)
-        for record in records:
-            if record.partner_id and record.survey_id:
-                # Tìm tất cả các bản ghi có cùng survey_id và partner_id, bao gồm cả bản ghi hiện tại
-                user_inputs = self.env['survey.user_input'].search([
-                    ('survey_id', '=', record.survey_id.id),
-                    ('partner_id', '=', record.partner_id.id)
-                ])
-                # Kiểm tra loại tính điểm và tìm điểm cao nhất
-                if record.survey_id.is_percentage_based:
-                    diem_cao_nhat = max(user_inputs.mapped('scoring_percentage'), default=0)
-                else:
-                    diem_cao_nhat = max(user_inputs.mapped('scoring_total'), default=0)
+    @api.depends('state')
+    def _compute_diem_cao_nhat(self):
+        for record in self:
+            if record.state == 'done':
+                if record.partner_id and record.survey_id:
+                    # Tìm tất cả các bản ghi có cùng survey_id và partner_id
+                    user_inputs = self.env['survey.user_input'].search([
+                        ('survey_id', '=', record.survey_id.id),
+                        ('partner_id', '=', record.partner_id.id),
+                        ('state', '=', 'done')
+                    ])
 
-                # Cập nhật diem_cao_nhat cho tất cả các bản ghi tìm thấy
-                user_inputs.write({'diem_cao_nhat': diem_cao_nhat})
+                    if user_inputs:
+                        if record.survey_id.is_percentage_based:
+                            # Tìm điểm cao nhất theo scoring_percentage
+                            diem_cao_nhat = max(user_inputs.mapped('scoring_percentage'), default=0)
+                        else:
+                            # Tìm điểm cao nhất theo scoring_total
+                            diem_cao_nhat = max(user_inputs.mapped('scoring_total'), default=0)
+
+                        # Cập nhật trường diem_cao_nhat cho tất cả các bản ghi tìm được
+                        user_inputs.write({'diem_cao_nhat': diem_cao_nhat})
+                else:
+                    # Nếu không tìm thấy partner_id hoặc survey_id thì gán diem_cao_nhat = 0
+                    record.diem_cao_nhat = 0
             else:
-                # Nếu không có partner_id hoặc survey_id, đặt diem_cao_nhat là 0
-                record.diem_cao_nhat = 0
+                pass
+
+    @api.depends('state')
+    def _compute_xep_loai(self):
         for record in self:
             classification = self.env['survey.classification'].search([
                 ('survey_id', '=', record.survey_id.id),
@@ -50,8 +59,6 @@ class SurveyUserInput(models.Model):
                 record.xep_loai = classification.name
             else:
                 record.xep_loai = 'Chưa xếp loại'
-        return records
-
 
     @api.depends('partner_id', 'survey_id')
     def _compute_so_lan_lam(self):
