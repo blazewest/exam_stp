92f898379fac60dcada37a392fd4261482d70ed7
Duy Hung <daoduyhung98@gmailcom>
2024-10-23 09:11:57 +0700

    first commit

--- survey_dienbien/models/survey_survey.py
+++ survey_dienbien/models/survey_survey.py
@@ -34,7 +34,7 @@ class SurveySurvey(models.Model):
     category_group_ids = fields.Many2many('category.question', string='Lĩnh vực câu hỏi')
     is_percentage_based = fields.Boolean(string="Xếp loại điểm theo %", default=True, help="Tích vào thì dựa theo % mà xếp loại, không tích thì dựa theo điểm số")
     classification_ids = fields.One2many(
-        'survey.classification', 'survey_id', string='Xếp loại điểm'
+        'survey.classification', 'survey_id', string='Xếp loại điểm', ondelete='cascade'
     )
     name_donvi = fields.Many2one('donvi', string='Đơn vị tổ chức')
 
